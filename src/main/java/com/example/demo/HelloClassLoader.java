6d43bdb149d8529e5ba9c6232c29def6d403ab17
Lina Ma <malina_magic@126.com>
2021-03-21 23:40:08 +0800

    week 1

---
+++ src/main/java/com/example/demo/HelloClassLoader.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+public class Demo extends ClassLoader {
+
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        Class clazz = new Demo().findClass("Hello");
+        clazz.getDeclaredMethod("hello").invoke(clazz.newInstance());
+    }
+
+    @Override
+    protected Class<?> findClass(String name) {
+        try {
+            byte[] bytes = FileUtils.readFileToByteArray(
+                    new File("/Users/malina/Developer/demo/Hello.xlass"));
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (255 - Byte.toUnsignedInt(bytes[i]));
+            }
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
